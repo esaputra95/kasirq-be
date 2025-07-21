2f847a0325b6210cffed404274e29153ed830534
Eko Saputra <Ekosaputra.t.i@gmail.com>
2025-07-21 23:46:56 +0700

    fix/filter store admin

--- src/mobile/controllers/masters/StoreController.ts
+++ src/mobile/controllers/masters/StoreController.ts
@@ -4,74 +4,92 @@ import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
 import moment from "moment";
 
-const getSelect = async (_req:Request, res:Response) => {
+const getSelect = async (_req: Request, res: Response) => {
     try {
-        let user
+        let user;
 
-        let dataOption:any=[]
-        if(res.locals.level==="cashier"){
+        let dataOption: any = [];
+        if (res.locals.level === "cashier") {
             user = await Model.users.findUnique({
                 where: {
                     id: res.locals.userId,
-                }
+                },
             });
             const data = await Model.stores.findMany({
                 where: {
-                    id: user?.storeId ?? '',
+                    id: user?.storeId ?? "",
                     expiredDate: {
-                        gte: moment().format()
-                    }
-                }
+                        gte: moment().format(),
+                    },
+                },
             });
             for (const value of data) {
-                dataOption= [
-                    ...dataOption, {
+                dataOption = [
+                    ...dataOption,
+                    {
                         key: value.id,
-                        value: value.name
-                    }
-                ]
+                        value: value.name,
+                    },
+                ];
+            }
+        } else if (res.locals.level === "owner") {
+            const data = await Model.stores.findMany({
+                where: {
+                    id: res.locals.userId ?? "",
+                    expiredDate: {
+                        gte: moment().format(),
+                    },
+                },
+            });
+            for (const value of data) {
+                dataOption = [
+                    ...dataOption,
+                    {
+                        key: value.id,
+                        value: value.name,
+                    },
+                ];
             }
         } else {
             const data = await Model.stores.findMany({
                 where: {
                     ownerId: res.locals.userId,
                     expiredDate: {
-                        gte: moment().format()
-                    }
-                }
+                        gte: moment().format(),
+                    },
+                },
             });
             for (const value of data) {
-                dataOption= [
-                    ...dataOption, {
+                dataOption = [
+                    ...dataOption,
+                    {
                         key: value.id,
-                        value: value.name
-                    }
-                ]
+                        value: value.name,
+                    },
+                ];
             }
         }
-        
+
         res.status(200).json({
             status: true,
-            message: 'successfully in get user data',
+            message: "successfully in get user data",
             data: {
-                store: dataOption
-            }
-        })
+                store: dataOption,
+            },
+        });
     } catch (error) {
         let message = {
-            status:500,
-            message: { msg: `${error}` }
-        }
+            status: 500,
+            message: { msg: `${error}` },
+        };
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-export { getSelect }
+export { getSelect };
