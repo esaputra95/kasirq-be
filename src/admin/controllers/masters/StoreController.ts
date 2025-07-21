99e30ea8c5f80820bed3d4ac9c87f378ffb8174c
Eko Saputra <Ekosaputra.t.i@gmail.com>
2025-07-21 23:46:56 +0700

    fix/filter store admin

--- src/admin/controllers/masters/StoreController.ts
+++ src/admin/controllers/masters/StoreController.ts
@@ -5,232 +5,236 @@ import { StoreQueryInterface } from "#root/interfaces/masters/StoreInterface";
 import Model from "#root/services/PrismaService";
 import { Prisma } from "@prisma/client";
 import { Request, Response } from "express";
-import { v4 as uuidv4 } from 'uuid';
+import { v4 as uuidv4 } from "uuid";
 
-const getSelect = async (req:Request, res:Response) => {
+const getSelect = async (req: Request, res: Response) => {
     try {
-        const owner:any = await getOwnerId(res.locals.userId, res.locals.level);
-        let filter:any={};
-        req.query.name ? filter={...filter, name: { contains: req.query?.name as string}} : null
-        
-        if(res.locals.level!=="owner" && res.locals.level!=="superadmin" && res.locals.level!=="admin") return res.status(401).json({status: false, data: []})
-        if(res.locals.level==="owner"){
-            filter={...filter, ownerId: owner.id}
+        const owner: any = await getOwnerId(
+            res.locals.userId,
+            res.locals.level
+        );
+        let filter: any = {};
+        req.query.name
+            ? (filter = {
+                  ...filter,
+                  name: { contains: req.query?.name as string },
+              })
+            : null;
+
+        if (
+            res.locals.level !== "owner" &&
+            res.locals.level !== "superadmin" &&
+            res.locals.level !== "admin"
+        )
+            return res.status(401).json({ status: false, data: [] });
+        if (res.locals.level === "owner") {
+            filter = { ...filter, ownerId: owner.id };
         }
-        let dataOption:any=[];
+        let dataOption: any = [];
         const data = await Model.stores.findMany({
-            // where: {
-            //     // ...filter,
-            // }
+            where: {
+                ...filter,
+            },
         });
         for (const value of data) {
-            dataOption= [
-                ...dataOption, {
+            dataOption = [
+                ...dataOption,
+                {
                     value: value.id,
-                    label: value.name
-                }
-            ]
+                    label: value.name,
+                },
+            ];
         }
         res.status(200).json({
             status: true,
-            message: 'successfully in get store select',
+            message: "successfully in get store select",
             data: {
-                store: dataOption
-            }
-        })
+                store: dataOption,
+            },
+        });
     } catch (error) {
         let message = {
-            status:500,
-            message: { msg: `${error}` }
-        }
+            status: 500,
+            message: { msg: `${error}` },
+        };
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-const getData = async (req:Request<{}, {}, {}, StoreQueryInterface>, res:Response) => {
+const getData = async (
+    req: Request<{}, {}, {}, StoreQueryInterface>,
+    res: Response
+) => {
     try {
         const query = req.query;
-        const owner:any = await getOwnerId(res.locals.userId, res.locals.userType)
+        const owner: any = await getOwnerId(
+            res.locals.userId,
+            res.locals.userType
+        );
         // PAGING
-        const take:number = parseInt(query.limit ?? 20 )
-        const page:number = parseInt(query.page ?? 1 );
-        const skip:number = (page-1)*take
+        const take: number = parseInt(query.limit ?? 20);
+        const page: number = parseInt(query.page ?? 1);
+        const skip: number = (page - 1) * take;
         // FILTER
-        let filter:any= []
-        query.name ? filter = [...filter, {name: { contains: query.name }}] : null
-        if(filter.length > 0){
+        let filter: any = [];
+        query.name
+            ? (filter = [...filter, { name: { contains: query.name } }])
+            : null;
+        if (filter.length > 0) {
             filter = {
-                OR: [
-                    ...filter
-                ]
-            }
+                OR: [...filter],
+            };
         }
         const data = await Model.stores.findMany({
             where: {
                 ...filter,
-                ownerId: owner.id
+                ownerId: owner.id,
             },
             skip: skip,
-            take: take
+            take: take,
         });
         const total = await Model.stores.count({
             where: {
                 ...filter,
-                ownerId: owner.id
-            }
-        })
+                ownerId: owner.id,
+            },
+        });
         res.status(200).json({
             status: true,
             message: "successful in getting Stores data",
             data: {
                 store: data,
-                info:{
+                info: {
                     page: page,
                     limit: take,
-                    total: total
-                }
-            }
-        })
+                    total: total,
+                },
+            },
+        });
     } catch (error) {
-        let message = errorType
-        message.message.msg = `${error}`
+        let message = errorType;
+        message.message.msg = `${error}`;
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-const postData = async (req:Request, res:Response) => {
+const postData = async (req: Request, res: Response) => {
     try {
-        const ownerId:any = await getOwnerId(res.locals.userId, res.locals.userType)
-        if(!ownerId.status) throw new Error('Owner not found')
-        const data = { ...req.body, id: uuidv4(), ownerId: ownerId.id};
+        const ownerId: any = await getOwnerId(
+            res.locals.userId,
+            res.locals.userType
+        );
+        if (!ownerId.status) throw new Error("Owner not found");
+        const data = { ...req.body, id: uuidv4(), ownerId: ownerId.id };
         delete data.storeId;
-        await Model.stores.create({data: data});
+        await Model.stores.create({ data: data });
         res.status(200).json({
             status: true,
-            message: 'successful in created Member data'
-        })
+            message: "successful in created Member data",
+        });
     } catch (error) {
-        let message = errorType
-        message.message.msg = `${error}`
+        let message = errorType;
+        message.message.msg = `${error}`;
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-const updateData = async (req:Request, res:Response) => {
+const updateData = async (req: Request, res: Response) => {
     try {
-        const data = { ...req.body};
+        const data = { ...req.body };
         await Model.stores.update({
             where: {
-                id: req.params.id
+                id: req.params.id,
             },
-            data: data
+            data: data,
         });
         res.status(200).json({
             status: true,
-            message: 'successful in updated Stores data'
-        })
+            message: "successful in updated Stores data",
+        });
     } catch (error) {
-        let message = errorType
-        message.message.msg = `${error}`
+        let message = errorType;
+        message.message.msg = `${error}`;
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-const deleteData = async (req:Request, res:Response)=> {
+const deleteData = async (req: Request, res: Response) => {
     try {
         await Model.stores.delete({
             where: {
-                id: req.params.id
-            }
-        })
+                id: req.params.id,
+            },
+        });
         res.status(200).json({
             status: false,
-            message: 'successfully in deleted Member data'
-        })
+            message: "successfully in deleted Member data",
+        });
     } catch (error) {
         let message = {
-            status:500,
-            message: { msg: `${error}` }
-        }
+            status: 500,
+            message: { msg: `${error}` },
+        };
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-const getDataById = async (req:Request, res:Response) => {
+const getDataById = async (req: Request, res: Response) => {
     try {
         const model = await Model.stores.findUnique({
             where: {
-                id: req.params.id
-            }
-        })
-        
-        if(!model) throw new Error('data not found')
+                id: req.params.id,
+            },
+        });
+
+        if (!model) throw new Error("data not found");
         res.status(200).json({
             status: true,
-            message: 'successfully in get Member data',
+            message: "successfully in get Member data",
             data: {
-                store: model
-            }
-        })
+                store: model,
+            },
+        });
     } catch (error) {
         let message = {
-            status:500,
-            message: { msg: `${error}` }
-        }
+            status: 500,
+            message: { msg: `${error}` },
+        };
         if (error instanceof Prisma.PrismaClientKnownRequestError) {
-            message =  await handleValidationError(error)
+            message = await handleValidationError(error);
         }
         res.status(500).json({
             status: message.status,
-            errors: [
-                message.message
-            ]
-        })
+            errors: [message.message],
+        });
     }
-}
+};
 
-export {
-    getData,
-    getDataById,
-    postData,
-    updateData,
-    getSelect,
-    deleteData,
-}
+export { getData, getDataById, postData, updateData, getSelect, deleteData };
