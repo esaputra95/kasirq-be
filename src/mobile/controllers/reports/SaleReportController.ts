6f99d4681fbac0932b14200d1013fa8eb7db6918
Eko Saputra <Ekosaputra.t.i@gmail.com>
2025-08-16 06:35:10 +0700

    update sales

--- src/mobile/controllers/reports/SaleReportController.ts
+++ src/mobile/controllers/reports/SaleReportController.ts
@@ -2,49 +2,63 @@ import Model from "#root/services/PrismaService";
 import { Request, Response } from "express";
 import moment from "moment";
 
-const getData = async (req:Request, res:Response) => {
+const getData = async (req: Request, res: Response) => {
     try {
-        let filter={};
+        let filter = {};
         const query = req.query;
-        query?.accountId ? filter = {accountCashId: req?.query?.accountId}:null
-        query?.storeId ? filter = {
-            ...filter,
-            storeId: query.storeId
-        } : null;
-        const start = moment(req.query?.start as string, 'YYYY-MM-DD').startOf('day').toDate();
-        const end = moment(req.query?.finish as string, 'YYYY-MM-DD').endOf('day').toDate();
-        
+        query?.accountId
+            ? (filter = { accountCashId: req?.query?.accountId })
+            : null;
+        query?.storeId
+            ? (filter = {
+                  ...filter,
+                  storeId: query.storeId,
+              })
+            : null;
+        query?.memberId
+            ? (filter = {
+                  ...filter,
+                  memberId: query.memberId,
+              })
+            : null;
+        const start = moment(req.query?.start as string, "YYYY-MM-DD")
+            .startOf("day")
+            .toDate();
+        const end = moment(req.query?.finish as string, "YYYY-MM-DD")
+            .endOf("day")
+            .toDate();
+
         const data = await Model.sales.findMany({
             include: {
                 members: true,
                 saleDetails: {
                     include: {
-                        products: true
-                    }
-                }
+                        products: true,
+                    },
+                },
             },
             where: {
                 date: {
                     gte: start,
                     lte: end,
                 },
-                ...filter
+                ...filter,
             },
             orderBy: [
                 {
-                    date: 'desc',
-                }, 
+                    date: "desc",
+                },
                 {
-                    transactionNumber: 'desc'
-                }
-            ]
+                    transactionNumber: "desc",
+                },
+            ],
         });
 
-        let totals=0
+        let totals = 0;
         for (const element of data) {
-            totals+=Number(element.total)
+            totals += Number(element.total);
         }
-        
+
         const total = await Model.sales.aggregate({
             _sum: {
                 total: true,
@@ -54,24 +68,24 @@ const getData = async (req:Request, res:Response) => {
                     gte: start,
                     lte: end,
                 },
-                ...filter
-            }
-        })
-        
+                ...filter,
+            },
+        });
+
         res.status(200).json({
             status: true,
-            message: 'Success get data sales report',
+            message: "Success get data sales report",
             data: {
                 sale: data,
-                total
-            }
-        })
+                total,
+            },
+        });
     } catch (error) {
         res.status(500).json({
             status: false,
-            message: `${error}`
-        })
+            message: `${error}`,
+        });
     }
-}
+};
 
-export { getData }
+export { getData };
