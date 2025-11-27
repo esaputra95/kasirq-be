3f27d54c31e2ecf6fa90f8095f1535038854c055
Eko Saputra <Ekosaputra.t.i@gmail.com>
2025-11-27 23:54:27 +0700

    feat: Add getDataCashiers endpoint and improve user service with cashier filtering logic

--- src/mobile/routers/masters/UserRoute.ts
+++ src/mobile/routers/masters/UserRoute.ts
@@ -1,14 +1,23 @@
 import express from "express";
-import { deleteData, getData, getDataById, postData, updateData, changePassword } from "#root/mobile/controllers/masters/UserController"
-import  userDataValidateSchemaBased from "#root/validations/masters/UserValidation";
+import {
+    deleteData,
+    getData,
+    getDataById,
+    postData,
+    updateData,
+    changePassword,
+    getDataCashiers,
+} from "#root/mobile/controllers/masters/UserController";
+import userDataValidateSchemaBased from "#root/validations/masters/UserValidation";
 import validationMessage from "#root/validations/Validate";
-const login = express.Router()
+const login = express.Router();
 
-login.get('/', getData);
-login.post('/',  validationMessage(userDataValidateSchemaBased), postData);
-login.put('/change-password', changePassword);
-login.put('/:id', updateData);
-login.delete('/:id', deleteData);
-login.get('/:id', getDataById);
+login.get("/", getData);
+login.get("/cashiers", getDataCashiers);
+login.post("/", validationMessage(userDataValidateSchemaBased), postData);
+login.put("/change-password", changePassword);
+login.put("/:id", updateData);
+login.delete("/:id", deleteData);
+login.get("/:id", getDataById);
 
-export default login
+export default login;
